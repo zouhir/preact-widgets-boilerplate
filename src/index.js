be0c451d7add7b841141320a1c1ebb4a87505f96
Zouhir Chahoud <zouhir@zouhir.org>
2017-03-04 02:11:42 +1100

    build bugfix

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-// import 'promise-polyfill';
-// import 'isomorphic-fetch';
-
-import habitat from './dev_modules';
+import habitat from 'preact-habitat';
 
 function init() {
-  console.log(typeof habitat)
+  console.log(typeof habitat);
   let Widget = require('./components/widget').default;
   let niceLogin = habitat(Widget);
-  niceLogin.render()
+  niceLogin.render();
 }
 
 // in development, set up HMR:
 if (module.hot) {
-  require('preact/devtools');   // enables React DevTools, be careful on IE
-  module.hot.accept('./components/widget', () => requestAnimationFrame(init) );
+  require('preact/devtools'); // enables React DevTools, be careful on IE
+  module.hot.accept('./components/widget', () => requestAnimationFrame(init));
 }
 
 init();
